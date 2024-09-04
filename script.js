37d65b98ebc7157b11ae58721a2c42c8389664c1
EuHttl <hyttalo2002@gmail.com>
2024-09-04 00:20:57 -0300

    add:carrousel_api

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    }, 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    keyboard: true,
+});
